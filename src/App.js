03f2b150fe01f9d7b3df4588ec5e755a1a1135a3
Aryan Pokharna <aryanpokharna@outlook.com>
2024-03-14 17:14:57 +1100

    selected date working, moment imported, calendar css moved to separate file

--- src/App.js
+++ src/App.js
@@ -4,34 +4,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // react wrapp
 import { faHeart } from '@fortawesome/free-solid-svg-icons'; // ind. comp.
 import './App.css';
 import './Calendar.css';
+import moment from 'moment';
 
 function MyYearlyCalendar() {
 
+  const currentYear = new Date().getFullYear();
+  const now = moment();
+  const tomorrow = moment().add(1, 'day');
+
   // State to store the selected dates
   const [selectedDates, setSelectedDates] = useState([]);
-
-  function onDatePicked(date) {
-    // alert(date);
-    // console.log(date._i)
-    // console.log(date)
-    setSelectedDates([...selectedDates, date]) // ... is the spread operator, creates clone of existing array + appends date
+  function newDatePicked(date) {
+    setSelectedDates(prevSelectedDates => [...prevSelectedDates, date]); // get the most recent state, ... is the spread operator, creates clone of existing array + appends date
     console.log(selectedDates)
   }
 
-  // Function to check if a date is selected
-  const isDateSelected = (date) => {
-    return selectedDates.includes(date);
+  // State to store the selected dates
+  const [pick, setPick] = useState(moment());
+  function setPicked(date) {
+    setPick(date);
+    newDatePicked(date);
+  }
+
+  // Function to toggle Week Separators
+  const [WeekSeparators, setWeekSeparators] = useState(false);
+  const toggleWeekSeparators = () => {
+    setWeekSeparators(!WeekSeparators);
   };
 
-  // Function to apply custom style to selected dates
-  const dateStyle = (date) => {
-    if (isDateSelected(date)) {
-      return { backgroundColor: 'orange' }; // Change the background color to orange for selected dates
-    }
-    return {}; // Return an empty object for other dates
+  // Function to toggle Date Range
+  const [DateRange, setDateRange] = useState(false);
+  const toggleDateRange = () => {
+    setDateRange(!DateRange);
   };
-
-  const currentYear = new Date().getFullYear();
 
   return (
     <div className="App">
@@ -43,14 +48,28 @@ function MyYearlyCalendar() {
         <h1>
           <input className="event-name" type="text" placeholder="Name your event"></input>
         </h1>
-        
+
         <h1>{currentYear}</h1>
         
         <Calendar 
-        year={currentYear}
-        onPickDate={onDatePicked}
-        customClasses={{date: dateStyle}} // Apply custom styles to dates
+          year={currentYear}
+          showWeekSeparators={WeekSeparators}
+          // selectRange={DateRange}
+          // selectedRange={[now, tomorrow]}
+          onPickDate={setPicked}
+          selectedDay={pick}
         />
+
+        <div className='week-separators'>
+          <p>Show Week Separators</p>
+          <button class="large-button" onClick={toggleWeekSeparators}>{WeekSeparators ? 'Hide' : 'Show'}</button>
+        </div>
+
+        <div className='week-separators'>
+          <p>Enable Date Range</p>
+          <button class="large-button" onClick={toggleDateRange}>{DateRange ? 'No' : 'Yes'}</button>
+        </div>
+        
 
       </body>
       
