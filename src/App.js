ede601cd0fff853db30e1e63d81a45ff9e1647ab
Aryan Pokharna <aryanpokharna@outlook.com>
2025-08-04 18:23:55 +1000

    random vibe coding

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
-import { Calendar } from 'react-yearly-calendar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // react wrapper
-import { faHeart } from '@fortawesome/free-solid-svg-icons'; // ind. comp.
-import './App.css';
-import './Calendar.css';
-import moment from 'moment';
+import React, { useState } from "react";
+import { Calendar } from "react-yearly-calendar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // react wrapper
+import { faHeart } from "@fortawesome/free-solid-svg-icons"; // ind. comp.
+import "./App.css";
+import "./Calendar.css";
+import moment from "moment";
 
 // #1 show all previously selected dates
 // #2 unselect previous dates
 // #3 range + unselects
 
 function MyYearlyCalendar() {
-
   const currentYear = new Date().getFullYear();
 
   // Function & State for date range
   const start = moment();
-  const finish = moment().add(1, 'day');
+  const finish = moment().add(1, "day");
 
   // State to store the selected dates
   const [multipleDates, setMultipleDates] = useState([]);
   function setMultiplePicked(date) {
-    setMultipleDates(prevSelectedDates => [...prevSelectedDates, date]); // get the most recent state, ... is the spread operator, creates clone of existing array + appends date
+    setMultipleDates((prevSelectedDates) => [...prevSelectedDates, date]); // get the most recent state, ... is the spread operator, creates clone of existing array + appends date
   }
 
   // State to store the selected dates
@@ -30,6 +29,12 @@ function MyYearlyCalendar() {
     setPick(date); // last selected date
     // toggleSelection(date); // all previously selected dates
     // console.log(selectedDates)
+  }
+
+  // NEW: State for multiple date ranges
+  const [selectedRanges, setSelectedRanges] = useState([]);
+  function handleMultipleRanges(newRange, currentRanges) {
+    setSelectedRanges([...currentRanges, newRange]);
   }
 
   // Remove date if already included
@@ -63,6 +68,12 @@ function MyYearlyCalendar() {
     setDateRange(!toggleRange);
   };
 
+  // NEW: Function + State to toggle Multiple Date Ranges
+  const [toggleMultipleRanges, setMultipleRanges] = useState(false);
+  const toggleMultipleDateRanges = () => {
+    setMultipleRanges(!toggleMultipleRanges);
+  };
+
   // Function to check if a date is selected
   const isSelected = (date) => {
     return true;
@@ -70,49 +81,88 @@ function MyYearlyCalendar() {
 
   return (
     <div className="App">
-
       <header className="App-header"></header>
-
       <body className="App-body">
-        
         <h1>
-          <input className="event-name" type="text" placeholder="Name your event"></input>
+          <input
+            className="event-name"
+            type="text"
+            placeholder="Name your event"
+          ></input>
         </h1>
-
         <h1>{currentYear}</h1>
-        
-        <Calendar 
+
+        <Calendar
           year={currentYear}
           showWeekSeparators={WeekSeparators}
           onPickDate={setPicked}
           selectedDay={pick}
-
           // Multiple single selected dates
           onPickDates={setMultiplePicked}
           selectedDays={multipleDates}
+          // NEW: Multiple date ranges
+          selectMultipleRanges={toggleMultipleRanges}
+          selectedRanges={selectedRanges}
+          onPickRanges={handleMultipleRanges}
 
           // onPickRange={setRange} // implement set range.
           // selectRange={toggleRange}
           // selectedRange={[start, finish]}
-          
         />
 
-        <div className='week-separators'>
+        <div className="week-separators">
           <p>Show Week Separators</p>
-          <button class="large-button" onClick={toggleWeekSeparators}>{WeekSeparators ? 'Hide' : 'Show'}</button>
+          <button class="large-button" onClick={toggleWeekSeparators}>
+            {WeekSeparators ? "Hide" : "Show"}
+          </button>
         </div>
 
-        <div className='week-separators'>
+        <div className="week-separators">
           <p>Enable Date Range</p>
-          <button class="large-button" onClick={toggleDateRange}>{toggleRange ? 'No' : 'Yes'}</button>
+          <button class="large-button" onClick={toggleDateRange}>
+            {toggleRange ? "No" : "Yes"}
+          </button>
         </div>
-        
 
+        {/* NEW: Multiple Date Ranges Toggle */}
+        <div className="week-separators">
+          <p>Enable Multiple Date Ranges</p>
+          <button class="large-button" onClick={toggleMultipleDateRanges}>
+            {toggleMultipleRanges ? "No" : "Yes"}
+          </button>
+        </div>
+
+        {/* NEW: Display Selected Ranges */}
+        {selectedRanges.length > 0 && (
+          <div className="selected-ranges">
+            <h3>Selected Date Ranges:</h3>
+            {selectedRanges.map((range, index) => (
+              <div key={index} className="range-item">
+                <span>
+                  {range[0].format("MMM DD, YYYY")} -{" "}
+                  {range[1].format("MMM DD, YYYY")}
+                </span>
+                <button
+                  className="remove-range"
+                  onClick={() => {
+                    const newRanges = selectedRanges.filter(
+                      (_, i) => i !== index
+                    );
+                    setSelectedRanges(newRanges);
+                  }}
+                >
+                  ×
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </body>
-      
-      <footer className="App-footer">Made with <FontAwesomeIcon icon={faHeart} /> by Aryan</footer>
+      <footer className="App-footer">
+        Made with <FontAwesomeIcon icon={faHeart} /> by Aryan
+      </footer>
     </div>
-  )
+  );
 }
 
 export default MyYearlyCalendar;
--- src/Calendar.js
+++ src/Calendar.js
@@ -1,40 +1,99 @@
-'use strict';
-
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+var _extends =
+  Object.assign ||
+  function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _createClass = (function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+})();
 
-var _react = require('react');
+var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
-var _propTypes = require('prop-types');
+var _propTypes = require("prop-types");
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
-var _moment = require('moment');
+var _moment = require("moment");
 
 var _moment2 = _interopRequireDefault(_moment);
 
-var _reactMomentProptypes = require('react-moment-proptypes');
+var _reactMomentProptypes = require("react-moment-proptypes");
 
-var _Month = require('./Month');
+var _Month = require("./Month");
 
 var _Month2 = _interopRequireDefault(_Month);
 
-var _utils = require('./utils');
+var _utils = require("./utils");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _possibleConstructorReturn(self, call) {
+  if (!self) {
+    throw new ReferenceError(
+      "this hasn't been initialised - super() hasn't been called"
+    );
+  }
+  return call && (typeof call === "object" || typeof call === "function")
+    ? call
+    : self;
+}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError(
+      "Super expression must either be null or a function, not " +
+        typeof superClass
+    );
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true,
+    },
+  });
+  if (superClass)
+    Object.setPrototypeOf
+      ? Object.setPrototypeOf(subClass, superClass)
+      : (subClass.__proto__ = superClass);
+}
 
 var propTypes = {
   year: _propTypes2.default.number.isRequired,
@@ -51,8 +110,18 @@ var propTypes = {
   onPickDates: _propTypes2.default.func, // Function for multiple selected dates
   selectedDays: _propTypes2.default.arrayOf(_reactMomentProptypes.momentObj), // Array of moment objects for multiple selected days
 
-  customClasses: _propTypes2.default.oneOfType([_propTypes2.default.object, _propTypes2.default.func]),
-  titles: _propTypes2.default.func
+  // New props for multiple date ranges
+  onPickRanges: _propTypes2.default.func, // Function for multiple date ranges
+  selectedRanges: _propTypes2.default.arrayOf(
+    _propTypes2.default.arrayOf(_reactMomentProptypes.momentObj)
+  ), // Array of range arrays
+  selectMultipleRanges: _propTypes2.default.bool, // Enable multiple range selection mode
+
+  customClasses: _propTypes2.default.oneOfType([
+    _propTypes2.default.object,
+    _propTypes2.default.func,
+  ]),
+  titles: _propTypes2.default.func,
 };
 
 var defaultProps = {
@@ -69,171 +138,248 @@ var defaultProps = {
   onPickDates: null, // Not sure why null.
   selectedDays: [], // No days are selected at initialisation.
 
+  // Default props for multiple ranges
+  onPickRanges: null,
+  selectedRanges: [], // No ranges are selected at initialisation
+  selectMultipleRanges: false,
+
   customClasses: null,
-  titles: null
+  titles: null,
 };
 
-var Calendar = function (_Component) {
+var Calendar = (function (_Component) {
   _inherits(Calendar, _Component);
 
   function Calendar(props) {
     _classCallCheck(this, Calendar);
 
-    var _this = _possibleConstructorReturn(this, (Calendar.__proto__ || Object.getPrototypeOf(Calendar)).call(this, props));
+    var _this = _possibleConstructorReturn(
+      this,
+      (Calendar.__proto__ || Object.getPrototypeOf(Calendar)).call(this, props)
+    );
 
     _this.state = {
-      selectingRange: undefined
+      selectingRange: undefined,
+      selectingMultipleRanges: undefined, // New state for multiple range selection
     };
     return _this;
   }
 
-  _createClass(Calendar, [{
-    key: 'dayClicked',
-    value: function dayClicked(date, classes) {
-      if (!date) {
-        // clicked on prev or next month
-        return;
-      }
-
-      var selectingRange = this.state.selectingRange;
-      var _props = this.props,
-          selectRange = _props.selectRange,
-          onPickRange = _props.onPickRange,
-          onPickDate = _props.onPickDate;
-          onPickDates = _props.onPickDates;
-
-
-      if (!selectRange && false) { // Disabled
-        if (onPickDate instanceof Function) {
-          onPickDate(date, classes); // Function passed through parent component i.e. App.js
+  _createClass(Calendar, [
+    {
+      key: "dayClicked",
+      value: function dayClicked(date, classes) {
+        if (!date) {
+          // clicked on prev or next month
+          return;
         }
-        return;
-      }
 
-      // Call onPickDates function with an array containing only the selected date and classes
-      if (!selectRange) {
+        var selectingRange = this.state.selectingRange;
+        var selectingMultipleRanges = this.state.selectingMultipleRanges;
+        var _props = this.props,
+          selectRange = _props.selectRange,
+          selectMultipleRanges = _props.selectMultipleRanges,
+          onPickRange = _props.onPickRange,
+          onPickDate = _props.onPickDate,
+          onPickDates = _props.onPickDates,
+          onPickRanges = _props.onPickRanges,
+          selectedRanges = _props.selectedRanges;
+
+        if (!selectRange && false) {
+          // Disabled
+          if (onPickDate instanceof Function) {
+            onPickDate(date, classes); // Function passed through parent component i.e. App.js
+          }
+          return;
+        }
+
+        // Handle multiple range selection mode
+        if (selectMultipleRanges) {
+          if (!selectingMultipleRanges) {
+            // Start a new range
+            selectingMultipleRanges = [date, date];
+            this.setState({
+              selectingMultipleRanges: selectingMultipleRanges,
+            });
+          } else {
+            // Complete the range
+            var startDate = selectingMultipleRanges[0];
+            var endDate = date;
+            if (startDate > date) {
+              endDate = selectingMultipleRanges[0];
+              startDate = date;
+            }
+
+            if (onPickRanges instanceof Function) {
+              onPickRanges([startDate, endDate], selectedRanges);
+            }
+
+            selectingMultipleRanges = undefined;
+            this.setState({
+              selectingMultipleRanges: selectingMultipleRanges,
+            });
+          }
+          return;
+        }
+
+        // Handle single range selection mode
+        if (selectRange) {
+          if (!selectingRange) {
+            selectingRange = [date, date];
+          } else {
+            if (onPickRange instanceof Function) {
+              if (selectingRange[0] > date) {
+                onPickRange(date, selectingRange[0]);
+              } else {
+                onPickRange(selectingRange[0], date);
+              }
+            }
+            selectingRange = undefined;
+          }
+
+          this.setState({
+            selectingRange: selectingRange,
+          });
+          return;
+        }
+
+        // Handle single date selection
+        if (onPickDate instanceof Function) {
+          onPickDate(date, classes);
+        }
+
+        // Handle multiple date selection
         if (onPickDates instanceof Function) {
           onPickDates([date], classes);
         }
-      }
-
-      if (!selectingRange) {
-        selectingRange = [date, date];
-      } else {
-        if (onPickRange instanceof Function) {
-          if (selectingRange[0] > date) {
-            onPickRange(date, selectingRange[0]);
-          } else {
-            onPickRange(selectingRange[0], date);
-          }
+      },
+    },
+    {
+      key: "dayHovered",
+      value: function dayHovered(hoveredDay) {
+        if (!hoveredDay) {
+          // clicked on prev or next month
+          return;
         }
-        selectingRange = undefined;
-      }
 
-      this.setState({
-        selectingRange: selectingRange
-      });
-    }
-  }, {
-    key: 'dayHovered',
-    value: function dayHovered(hoveredDay) {
-      if (!hoveredDay) {
-        // clicked on prev or next month
-        return;
-      }
+        var selectingRange = this.state.selectingRange;
+        var selectingMultipleRanges = this.state.selectingMultipleRanges;
+        var selectMultipleRanges = this.props.selectMultipleRanges;
 
-      var selectingRange = this.state.selectingRange;
-      
-      if (selectingRange) {
-        selectingRange[1] = hoveredDay;
+        // Handle multiple range selection hover
+        if (selectMultipleRanges && selectingMultipleRanges) {
+          selectingMultipleRanges[1] = hoveredDay;
+          this.setState({
+            selectingMultipleRanges: selectingMultipleRanges,
+          });
+          return;
+        }
 
-        this.setState({
-          selectingRange: selectingRange
-        });
-      }
-    }
-  }, {
-    key: 'renderDaysOfWeek',
-    value: function renderDaysOfWeek() {
-      var _props2 = this.props,
+        // Handle single range selection hover
+        if (selectingRange) {
+          selectingRange[1] = hoveredDay;
+
+          this.setState({
+            selectingRange: selectingRange,
+          });
+        }
+      },
+    },
+    {
+      key: "renderDaysOfWeek",
+      value: function renderDaysOfWeek() {
+        var _props2 = this.props,
           useIsoWeekday = _props2.useIsoWeekday,
           firstDayOfWeek = _props2.firstDayOfWeek,
           forceFullWeeks = _props2.forceFullWeeks,
           showWeekSeparators = _props2.showWeekSeparators;
 
-      var totalDays = forceFullWeeks ? 42 : 37;
+        var totalDays = forceFullWeeks ? 42 : 37;
 
-      var days = [];
-      (0, _utils.range)(firstDayOfWeek, totalDays + firstDayOfWeek).forEach(function (i) {
-        var momentDay = useIsoWeekday ? (0, _moment2.default)().isoWeekday(i) : (0, _moment2.default)().weekday(i);
-        var day = momentDay.format('ddd').charAt(0);
+        var days = [];
+        (0, _utils.range)(firstDayOfWeek, totalDays + firstDayOfWeek).forEach(
+          function (i) {
+            var momentDay = useIsoWeekday
+              ? (0, _moment2.default)().isoWeekday(i)
+              : (0, _moment2.default)().weekday(i);
+            var day = momentDay.format("ddd").charAt(0);
 
-        if (showWeekSeparators) {
-          if (i % 7 === firstDayOfWeek && days.length) {
-            // push week separator
-            days.push(_react2.default.createElement('th', { className: 'week-separator', key: 'seperator-' + i }));
+            if (showWeekSeparators) {
+              if (i % 7 === firstDayOfWeek && days.length) {
+                // push week separator
+                days.push(
+                  _react2.default.createElement("th", {
+                    className: "week-separator",
+                    key: "seperator-" + i,
+                  })
+                );
+              }
+            }
+            days.push(
+              _react2.default.createElement(
+                "th",
+                { key: "weekday-" + i, className: i % 7 === 0 ? "bolder" : "" },
+                day
+              )
+            );
           }
-        }
-        days.push(_react2.default.createElement(
-          'th',
-          { key: 'weekday-' + i, className: i % 7 === 0 ? 'bolder' : '' },
-          day
-        ));
-      });
+        );
 
-      return _react2.default.createElement(
-        'tr',
-        null,
-        _react2.default.createElement(
-          'th',
+        return _react2.default.createElement(
+          "tr",
           null,
-          '\xA0'
-        ),
-        days
-      );
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
+          _react2.default.createElement("th", null, "\xA0"),
+          days
+        );
+      },
+    },
+    {
+      key: "render",
+      value: function render() {
+        var _this2 = this;
 
-      var selectingRange = this.state.selectingRange;
+        var selectingRange = this.state.selectingRange;
+        var selectingMultipleRanges = this.state.selectingMultipleRanges;
 
+        var months = (0, _utils.range)(0, 12).map(function (month) {
+          return _react2.default.createElement(
+            _Month2.default,
+            _extends(
+              {
+                month: month,
+                key: "month-" + month,
+                dayClicked: function dayClicked(d, classes) {
+                  return _this2.dayClicked(d, classes);
+                },
+                dayHovered: function dayHovered(d) {
+                  return _this2.dayHovered(d);
+                },
+              },
+              _this2.props,
+              {
+                selectingRange: selectingRange,
+                selectingMultipleRanges: selectingMultipleRanges,
+              }
+            )
+          );
+        });
 
-      var months = (0, _utils.range)(0, 12).map(function (month) {
-        return _react2.default.createElement(_Month2.default, _extends({
-          month: month,
-          key: 'month-' + month,
-          dayClicked: function dayClicked(d, classes) {
-            return _this2.dayClicked(d, classes);
-          },
-          dayHovered: function dayHovered(d) {
-            return _this2.dayHovered(d);
-          }
-        }, _this2.props, {
-          selectingRange: selectingRange
-        }));
-      });
-
-      return _react2.default.createElement(
-        'table',
-        { className: 'calendar' },
-        _react2.default.createElement(
-          'thead',
-          { className: 'day-headers' },
-          this.props.showDaysOfWeek ? this.renderDaysOfWeek() : null
-        ),
-        _react2.default.createElement(
-          'tbody',
-          null,
-          months
-        )
-      );
-    }
-  }]);
+        return _react2.default.createElement(
+          "table",
+          { className: "calendar" },
+          _react2.default.createElement(
+            "thead",
+            { className: "day-headers" },
+            this.props.showDaysOfWeek ? this.renderDaysOfWeek() : null
+          ),
+          _react2.default.createElement("tbody", null, months)
+        );
+      },
+    },
+  ]);
 
   return Calendar;
-}(_react.Component);
+})(_react.Component);
 
 Calendar.propTypes = propTypes;
 Calendar.defaultProps = defaultProps;
